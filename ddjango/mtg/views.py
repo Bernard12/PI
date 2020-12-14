aa680499d1e52b6521b66c211bcb9be537922996
ivan <ivnsimakhin@gmail.com>
2020-12-14 15:33:10 +0000

    (#3) Process only needed http methods (GET/POST)

--- ddjango/mtg/views.py
+++ ddjango/mtg/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_http_methods
 
+@require_http_methods(["GET", "POST"])
 def customer_profile(req, id = None):
     return JsonResponse({ 'name': 'John Doe', 'purchased_cards': [] })
 
+@require_http_methods(["GET", "POST"])
 def card_profile(req, id = None):
     return JsonResponse({ 'Title': 'Simple name', 'author': 'John doe', 'image_url': 'https://example.com', 'description': { 'message': 'Wow', 'author': 'somebody' } })
 
+@require_http_methods(["GET", "POST"])
 def cards_list(req):
     return JsonResponse({ 'cards': [] })
 
+@require_http_methods(["GET", "POST"])
 def cards_list_by_category(req, category = None):
     return JsonResponse({ 'cards': [] })
