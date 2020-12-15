67eb547cf697aaa35917663a2da7cb3b775bd0c9
ivan <ivnsimakhin@gmail.com>
2020-12-15 15:00:39 +0000

    (misc) Added more admins class

--- ddjango/card_color/admin.py
+++ ddjango/card_color/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from card_color.models import CardColor
 
 # Register your models here.
+class CardColorAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CardColor, CardColorAdmin)
