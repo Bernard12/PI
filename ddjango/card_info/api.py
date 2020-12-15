dbc5b41c4a1cef02a334fcde8b4886a268e00a33
ivan <ivnsimakhin@gmail.com>
2020-12-15 17:10:36 +0000

    (#5) Added method for card creation

--- ddjango/card_info/api.py
+++ ddjango/card_info/api.py
@@ -1,4 +1,5 @@
 from card_info.models import CardInfo
+from card_color.models import CardColor
 
 def get_card(id = 0):
     return CardInfo.objects.get(id=id)
@@ -8,3 +9,18 @@ def get_all_cards():
 
 def get_cards_by_color(color=''):
     return CardInfo.objects.filter(colors__card_color__exact=color)
+
+def create_card(title='', author='', image_url='', expansion='', card_type='', color=[], lore_message=None, lore_author=None):
+    card_colors = CardColor.objects.filter(card_color__in=color)
+    card = CardInfo.objects.create(
+        title=title,
+        author=author,
+        image_url=image_url,
+        expansion=expansion,
+        type=card_type,
+        lore_message=lore_message,
+        lore_author=lore_author,
+    )
+    for col in card_colors:
+        print(col)
+        card.colors.add(col)
