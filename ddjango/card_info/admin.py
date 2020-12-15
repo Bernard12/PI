d8cead1981a6b6f05736940368441f5e0891bb1d
ivan <ivnsimakhin@gmail.com>
2020-12-15 17:19:02 +0000

    (misc) Update lsit display for card info

--- ddjango/card_info/admin.py
+++ ddjango/card_info/admin.py
@@ -3,6 +3,9 @@ from card_info.models import CardInfo
 
 # Register your models here.
 class CardInfoAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'expansion', 'type', 'get_colors')
+
+    def get_colors(self, obj):
+        return ' '.join([p.card_color for p in obj.colors.all()])
 
 admin.site.register(CardInfo, CardInfoAdmin)
