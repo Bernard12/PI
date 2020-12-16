f9af81cdf60b75bab830528a36273c0f683c5a95
ivan <ivnsimakhin@gmail.com>
2020-12-16 09:19:49 +0000

    (misc) Extracted card color choices

--- ddjango/card_color/models.py
+++ ddjango/card_color/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
+WHITE = 'white'
+BLUE = 'blue'
+RED = 'red'
+GREEN = 'green'
+BLACK = 'black'
+COLOR_CHOICES = [
+    (WHITE, WHITE),
+    (BLUE, BLUE),
+    (RED, RED),
+    (GREEN, GREEN),
+    (BLACK, BLACK),
+]
+
 # Create your models here.
 class CardColor(models.Model):
-    WHITE = 'white'
-    BLUE = 'blue'
-    RED = 'red'
-    GREEN = 'green'
-    BLACK = 'black'
-    COLOR_CHOICES = [
-        (WHITE, WHITE),
-        (BLUE, BLUE),
-        (RED, RED),
-        (GREEN, GREEN),
-        (BLACK, BLACK),
-    ]
-
     card_color = models.CharField(max_length=5, choices=COLOR_CHOICES)
 
 
